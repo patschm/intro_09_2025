09bba2606250478cbf90150da02da4bf37ffa3fc
Patrick Schmidt <pschmidt@pasit.nl>
2025-09-22 11:54:25 +0200

    init

---
+++ Live/Module_2/D1_Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace D1_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hier mag ik variabelen declareren.
+            byte age;
+            age = 2; // Literal
+            long yearsBeforeEarth;
+            //checked
+            //{
+                int ages = int.Parse("1000") * 3_000_000;
+                Console.WriteLine(ages);
+            //}
+
+            char kar = 'a'; // Literal
+            string name = "Patrick";
+            Console.WriteLine(name);
+            Console.WriteLine("Geef uw naam");
+            name = Console.ReadLine();
+            Console.WriteLine(name);
+        }
+    }
+}
